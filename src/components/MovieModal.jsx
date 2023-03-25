1d63605940085d658743bf4f07844f2b611c6516
Armando Valencia <javm8@yahoo.com>
2023-03-25 13:22:10 -0500

    Fetch data from api endpoint and render to modal

--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -7,9 +7,15 @@ import {
     ModalOverlay,
 } from '@chakra-ui/react';
 
-const MovieModal = ({ isModalOpen, onClose, setIsModalOpen }) => {
-    let movieTitle;
-    let movieDesc;
+let movieTitle;
+let movieDesc;
+
+const MovieModal = ({ isModalOpen, onClose, setIsModalOpen, data }) => {
+    if (data !== null) {
+        let movieIndex = data[Math.floor(Math.random() * data.length)];
+        movieTitle = movieIndex.title;
+        movieDesc = movieIndex.overview;
+    }
 
     return (
         <>
@@ -22,15 +28,21 @@ const MovieModal = ({ isModalOpen, onClose, setIsModalOpen }) => {
                         padding={4}
                         marginTop={10}
                     >
-                        <h1>{movieTitle ? movieTitle : 'test title'}</h1>
-                        <h2>{movieDesc ? movieDesc : 'test desc'}</h2>
+                        {data !== null && (
+                            <>
+                                <h1 className="text-lg font-bold">
+                                    {movieTitle ? movieTitle : 'test title'}
+                                </h1>
+                                <h2>{movieDesc ? movieDesc : 'test desc'}</h2>
+                            </>
+                        )}
                     </ModalBody>
 
                     <ModalFooter placeContent="center" paddingTop={10}>
                         <Button
                             variant="outlined"
                             colorScheme="brand"
-                            onClick={e => setIsModalOpen(false)}
+                            onClick={() => setIsModalOpen(false)}
                         >
                             Close
                         </Button>
