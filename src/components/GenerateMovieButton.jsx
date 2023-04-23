6a31e3d0bc4e126dab9946cd5eb16baedde2b8e7
Armando Valencia <javm8@yahoo.com>
2023-04-22 20:22:05 -0500

    Add pagination of search results

--- src/components/GenerateMovieButton.jsx
+++ src/components/GenerateMovieButton.jsx
@@ -1,53 +1,17 @@
 import { Button } from '@chakra-ui/react';
-import { useEffect } from 'react';
-import useFetchMovie from '../hooks/useFetchMovie';
 
 // selectedGenreId,
-const GenerateMovieButton = ({
-    url,
-    setIsModalOpen,
-    setData,
-    setIsLoading,
-}) => {
+const GenerateMovieButton = ({ setIsModalOpen, fetchData }) => {
     // fullUrl = selectedGenreId ? `${url}&genre=${selectedGenreId}` : url;
-
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
-            'X-RapidAPI-Host': import.meta.env.VITE_API_HOST,
-        },
-    };
-
-    useEffect(() => {
-        useFetchMovie(url, options);
-    }, []);
-
-    const fetchData = async () => {
-        try {
-            let response = await fetch(url, options);
-            let data = await response.json();
-            setData(data.result);
-            console.log(`data: `, data.result);
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    const callFetch = async () => {
-        setIsModalOpen(true);
-        setIsLoading(true);
-
-        await fetchData();
-
-        setIsLoading(false);
-    };
 
     return (
         <Button
             variant="outlined"
             colorScheme="brand"
-            onClick={e => callFetch()}
+            onClick={e => {
+                setIsModalOpen(true);
+                fetchData();
+            }}
         >
             Generate!
         </Button>
